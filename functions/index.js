433aa4d7496a8058142102d1313086cab3102a12
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-28 16:31:14 +0500

    .

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onChange(event => {
+exports.onFileChange = functions.storage.object().onMetadataUpdate(event => {
   console.log(event);
   return;
 });
